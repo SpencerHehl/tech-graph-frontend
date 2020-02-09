a9a29ef910a1fa3c5fad3417fbc3581296c77249
Spencer Hehl <schehl49@gmail.com>
2020-02-09 11:16:54 -0500

    initial service list

--- src/app/service-list/service-list.component.ts
+++ src/app/service-list/service-list.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./service-list.component.scss']
 })
 export class ServiceListComponent implements OnInit {
-
+  serviceList = [
+    [
+      {
+        name: 'Telemetry Service',
+        description: 'The service responsible for providing telemetry data to a given front end'
+      },
+      {
+        name: 'Fleet Logistics Service',
+        description: 'The service responsible for managing fleet system logistics including customer, device, and point information.'
+      }
+    ], [
+      {
+        name: 'State Service',
+        description: 'The service responsible for managing the cumulative state of fleet devices.'
+      },
+      {
+        name: 'Airsapce Service',
+        description: 'The service responsible for monitoring Airspace traffic and advisories and providing flight status based on the data.'
+      }
+    ]
+  ]
   constructor() { }
 
   ngOnInit() {
